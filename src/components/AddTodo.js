f4fdb50ad54031a6a41ef73691d8a96e4bd7f6ae
siddhant <siddhant.chawla9@gmail.com>
2020-04-28 21:31:02 +0530

    PropTypes added

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class AddTodo extends Component {
     state = {
@@ -39,5 +40,9 @@ export class AddTodo extends Component {
         )
     }
 }
+
+TodoItem.propTypes = {
+    addTodo : PropTypes.func.isRequired
+  }
 
 export default AddTodo
